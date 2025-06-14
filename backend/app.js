f1ffde4e9c686e24178e8caf917c7b4c3bf735bb
Thiyagarajan <thiyagarajankrishnamoorthyk@gmail.com>
2025-06-14 05:33:06 +0530

    Initial Commit with Session

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-auth-token']
 }));
 
